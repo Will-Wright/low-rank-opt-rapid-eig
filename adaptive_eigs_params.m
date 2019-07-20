8f3cf6d34424534a9f0fddb9ec4ac6ef558c9933
Will-Wright <willwright@math.ucdavis.edu>
2019-07-19 19:10:09 -0700

    updates adaptive_eigs_params comments

--- adaptive_eigs_params.m
+++ adaptive_eigs_params.m
@@ -1,6 +1,11 @@
-function [num_eigs_update] = adaptive_eigs_params(num_matvecs, num_eigs, iter, opts)
-% [num_eigs] = adaptive_eigs_params(num_eigs, num_matvecs)
-% Determine new num_eigs (parameter `k` for `eigs`)
+function [num_eigs_new] = adaptive_eigs_params(num_matvecs, num_eigs, iter, opts)
+% [num_eigs_new] = adaptive_eigs_params(num_matvecs, num_eigs, iter, opts)
+% Determines new parameter num_eigs (parameter `k` for `eigs`)
+% based on previous results from `eigs`
+%
+% See explanation in repository readme or Chapter 6
+% of the dissertation
+% https://github.com/Will-Wright/dissertation-rapid_eigenvalue_method_for_noisy_phase_retrieval/blob/master/will_wright_dissertation.pdf
 
 num_eigs_min = opts.num_eigs_min;
 num_eigs_max = opts.num_eigs_max;
@@ -10,16 +15,16 @@ num_matvecs = num_matvecs(:);
 num_eigs = num_eigs(:);
 
 if length(num_eigs) == 1 || num_eigs(iter) == num_eigs_min
-   num_eigs_update = num_eigs(iter,1) + 1;
+   num_eigs_new = num_eigs(iter,1) + 1;
 elseif num_eigs(iter) == num_eigs_max
-   num_eigs_update = num_eigs(iter,1) - 1;
+   num_eigs_new = num_eigs(iter,1) - 1;
 elseif iter < num_lin_interp_pts
    delta_2 = sign( num_eigs(iter, 1) - num_eigs(iter-1, 1) )...
       *sign( num_matvecs(iter-1, 1) - num_matvecs(iter, 1) );
    if delta_2 == 0
       delta_2 = sign( num_eigs(iter, 1) - num_eigs(iter-1, 1) );
    end
-   num_eigs_update = num_eigs(iter, 1) + delta_2;
+   num_eigs_new = num_eigs(iter, 1) + delta_2;
 else
    delta_2 = sign( num_eigs(iter, 1) - num_eigs(iter-1, 1) )...
       *sign( num_matvecs(iter-1) - num_matvecs(iter) );
@@ -38,8 +43,8 @@ else
       delta = delta_2;
    end
    
-   num_eigs_update = num_eigs(iter, 1) + delta;
-   num_eigs_update = min( max(num_eigs_update, num_eigs_min), num_eigs_max);
+   num_eigs_new = num_eigs(iter, 1) + delta;
+   num_eigs_new = min( max(num_eigs_new, num_eigs_min), num_eigs_max);
 end
 
 
